49cad8d02098e45dcc114be0a36b6868f910b089
Andrew McIntosh <andrewdavidmcintosh@gmail.com>
2018-01-08 19:06:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function theBeatlesPlay(musicians, instruments) {
   return whatTheyPlay;
 }
 
-function johnLennonFacts(facts) {
+function johnLennonFacts() {
   while (facts) {
     return facts + "!!!";
   }
